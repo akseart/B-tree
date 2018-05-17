d67047f2a45e6e2bb4ee51fb8e8e9ff83e873bf3
akseart <akseart@ya.ru>
2018-05-17 20:05:12 +0300

    Initial Commit

---
+++ B-tree/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  B-tree
+//
+//  Created by Артем on 17.05.2018.
+//  Copyright © 2018 Артем. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
